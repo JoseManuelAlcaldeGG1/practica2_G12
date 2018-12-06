1eebc15dcac413a7548f03a2032fc950d96bb8e9
Javier Herrero Porras <i72hepoj@uco.es>
2018-12-06 17:22:39 +0100

    Subiendo fichero de cabecera de alumno

---
+++ alumno.h
@@ -0,0 +1,29 @@
+
+#ifndef ALUMNO_H_
+#define ALUMNO_H_
+
+#include <iostream>
+#include "persona.h"
+
+class Alumno: public Persona{
+
+	private:
+		int Equipo_;
+		bool Lider_;
+		int CursoMasAlto_;
+
+	public:
+		Alumno(int Equipo, bool Lider, int CursoMasAlto, string DNI, string Email, string Nombre, string Apellido1, string Apellido2, string Telefono, string DireccionPostal, string FechaNacimiento):Persona(DNI, Email, Nombre, Apellido1, Apellido2, Telefono, DireccionPostal, FechaNacimiento)
+		{Equipo_= Equipo; Lider_=Lider; CursoMasAlto_=CursoMasAlto;};
+
+		inline int getEquipo() {return Equipo_;};
+		inline void setEquipo(int Equipo) {Equipo_=Equipo;};
+
+		bool getLider();
+		inline void setLider(bool Lider) {Lider_=Lider;};
+
+		inline int getCursoMasAlto() {return CursoMasAlto_;};
+		inline void setCursoMasAlto(int CursoMasAlto) {CursoMasAlto_= CursoMasAlto;};
+};
+
+#endif
