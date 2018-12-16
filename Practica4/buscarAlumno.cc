d5d9a1c4f39eba8227d7a7a8678bc3833b607a22
Jose Manuel Alcalde <i72alllj@uco.es>
2018-12-16 20:35:25 +0100

    Buscar alumno

---
+++ Practica4/buscarAlumno.cc
@@ -0,0 +1,50 @@
+#include "agenda.h"
+#include "alumno.h"
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+Alumno Agenda::BuscarAlumno(string cad, int opcion){
+	
+	Alumno fallo(-1,0,0,"0","0","0","0","0","0","0","0");
+
+
+
+	switch(opcion){
+
+		case 1:
+		{	
+			list <Alumno>::iterator it;
+								
+			for(it=ListaAlumnos_.begin();it!=ListaAlumnos_.end();it++){
+				
+				if(it->getDNI()==cad){
+					return (*it);
+				}
+			}
+							
+			return fallo; //HAY QUE AJUSTAR ESTO EN LA FUNCION QUE LO USE PARA QUE IMPRIMA QUE NO SE HA ENCONTRADO EL ALUMNO
+			break;
+			}
+		case 2:
+		{
+			list <Alumno>::iterator it2;
+			
+			for(it2=ListaAlumnos_.begin();it2!=ListaAlumnos_.end();it2++){
+				
+				if((it2->getApellido1()+" "+it2->getApellido2())==cad){
+					return (*it2);
+				}
+			}
+			
+			return fallo;////HAY QUE AJUSTAR ESTO EN LA FUNCION QUE LO USE PARA QUE IMPRIMA QUE NO SE HA ENCONTRADO EL ALUMNO
+			break;
+		}
+
+			
+	}
+
+	
+}
