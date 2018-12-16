b599ff75c3b3edb1fe6c73b923b4532c24aa6448
Javier Herrero Porras <i72hepoj@uco.es>
2018-12-16 20:21:11 +0100

    Añadiendo el fichero de cabecera de funciones

---
+++ Practica4/funciones.h
@@ -0,0 +1,12 @@
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+#include <string>
+#include <cstring>
+using namespace std;
+
+bool existeFichero(char* nfichero);
+bool compruebaDNI(string DNI);
+bool compruebaEmail(string Email);
+
+#endif
